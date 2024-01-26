98d766efb5673b22b8251b8411022001c50858d0
hyunki <sjy6149@gmail.com>
2024-01-26 11:11:04 +0900

    유저 권한 정리

--- ERD/sample.sql
+++ ERD/sample.sql
@@ -5,6 +5,18 @@ select * from a_user;
 
 select * from a_testinfo;
 
+select * from a_authority;
+
+ALTER TABLE a_user DROP CONSTRAINT UK_3nstbuhppkiorpc6ms42mluut;
+
+ALTER TABLE a_authority
+    DROP CONSTRAINT UK_sq6lwmnd95tb0kc75s1tjqrs5;
+
+DROP TABLE user_authority;
+
+ALTER TABLE user_authority DROP CONSTRAINT FK4ojeblvl273tr6bx50tcg3awn;
+
+
 
 
 
--- src/main/java/com/lec/spring/domain/BaseEntity.java
+++ src/main/java/com/lec/spring/domain/BaseEntity.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PreUpdate;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.domain.Auditable;
@@ -20,4 +21,12 @@ public class BaseEntity {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     @JsonProperty("create_time")
     private LocalDateTime createTime;
+
+    // 업데이트 시에 create_time이 변경되지 않도록 함
+    @PreUpdate
+    public void preUpdate() {
+        if (this.createTime == null) {
+            this.createTime = LocalDateTime.now();
+        }
+    }
 }
