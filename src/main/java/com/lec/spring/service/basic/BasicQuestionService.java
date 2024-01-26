5eb75b8f585a56525db032edbad853d15900f093
hyunki <sjy6149@gmail.com>
2024-01-26 17:39:23 +0900

    전체 정렬값 수정

--- src/main/java/com/lec/spring/service/basic/BasicQuestionService.java
+++ src/main/java/com/lec/spring/service/basic/BasicQuestionService.java
@@ -3,6 +3,7 @@ package com.lec.spring.service.basic;
 import com.lec.spring.domain.basic.BasicQuestion;
 import com.lec.spring.repository.basic.BasicQuestionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,7 +17,7 @@ public class BasicQuestionService {
     public BasicQuestion save(BasicQuestion basicQuestion) {return basicQuestionRepository.save(basicQuestion);}
     public BasicQuestion update(BasicQuestion basicQuestion) {return basicQuestionRepository.save(basicQuestion);}
     public BasicQuestion findById(Long id) {return basicQuestionRepository.findById(id).orElse(null);}
-    public List<BasicQuestion> findAll() {return basicQuestionRepository.findAll();}
+    public List<BasicQuestion> findAll() {return basicQuestionRepository.findAll(Sort.by(Sort.Order.desc("id")));}
     public String delete(Long id) {
         if(id != null){
         basicQuestionRepository.deleteById(id);
